d5ae78af33dc754edca9dd37924cee54e45d50b9
tamy <tamybellaksu@gmail.com>
2022-01-30 15:25:57 +0200

    commit list to do in AngularJs

--- app.js
+++ app.js
@@ -1,11 +1,35 @@
-(function(){
-'use strict';
-var x="d";
+
+
+
  angular.module('myFirstApp', [])
 
- .controller('MyFirstController',function(){
+.controller('ToDoListController', function ($scope){
+    $scope.item1 ='';
+$scope.arr =[
+    {
+    'content': ''
+    }
+];
+$scope.addItem =function(){
+    if( $scope.item1=="")
+    {
+        return;
+    }
+    $scope.arr.push({'content':$scope.item1});
+    $scope.item1="";
+}
+
+$scope.deleteItem = function(id){
+    $scope.arr.map((v,i)=>{
+        if(i == id){
+            $scope.arr.splice(id,1);
+        }
+    }
+    )
+}
+
 
 });
 
-})();
+
  
